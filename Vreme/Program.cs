607bedaf8ed8b15255781214d9a44b46c4ddfaf8
Dragan Pepic <d.pepic@hotmail.com>
2021-10-11 10:11:00 +0200

    Add project files.

---
+++ Vreme/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Vreme
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		
+			DateTime vreme = DateTime.Now;
+			DateTime vreme2 = vreme.AddMinutes(60);
+			Console.WriteLine(vreme);
+			Console.WriteLine(vreme.Date);
+			
+			if (vreme.Date == vreme2.Date)
+				Console.WriteLine("jednaki su");
+			else
+				Console.WriteLine("nisu");
+			Console.WriteLine(vreme.TimeOfDay.TotalHours);
+
+			TimeSpan ts = new TimeSpan(6, 0, 0, 0);
+
+			DateTime novi = DateTime.Now + ts;
+			Console.WriteLine(novi);
+		}
+	}
+}
